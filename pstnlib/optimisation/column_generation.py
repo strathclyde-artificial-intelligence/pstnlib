34376b4cd977d5692a3034b52b4da2da1d8c7b2c
Andrew Murray <kpb20194@pd-michae2.cis.strath.ac.uk>
2022-11-01 23:01:12 +0000

    fixed verbosity check

--- pstnlib/optimisation/column_generation.py
+++ pstnlib/optimisation/column_generation.py
@@ -29,7 +29,7 @@ class PstnOptimisation(object):
         """
         Parses the probablistic temporal network and generates initial approximation points.
         """
-        if self.verbose not in [0, 1, 2]:
+        if verbose not in [0, 1, 2]:
             raise ValueError("Invalid verbosity level. Use 0 for no log, 1 for info and 2 for info and debug.")
         self.verbose = verbose
         logging.basicConfig(filename=logfile, encoding='utf-8', level=logging.DEBUG) if self.verbose == True else None
